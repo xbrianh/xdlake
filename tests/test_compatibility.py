87cb0f312014129a3b702027f1971f5e2d7d9f84
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2024-08-23 09:05:53 -0600

    Improve deltalake compatibility tests

--- tests/test_compatibility.py
+++ tests/test_compatibility.py
@@ -14,44 +14,45 @@ class TestCompatibility(TableGenMixin, unittest.TestCase):
         self.partition_by = list(self.table_gen.categoricals.keys())
 
     def test_append_and_overwrite(self):
-        loc = f"{self.scratch_folder}/{uuid4()}"
+        xd_loc = f"{self.scratch_folder}/{uuid4()}"
+        dt_loc = f"{self.scratch_folder}/{uuid4()}"
+        arrow_tables = [self.gen_table() for _ in range(3)]
+        overwrite_arrow_table = self.gen_table()
 
-        for _ in range(3):
-            xdlake.Writer.write(loc, self.gen_table(), partition_by=self.partition_by)
+        for at in arrow_tables:
+            xdlake.Writer.write(xd_loc, at, partition_by=self.partition_by)
+            deltalake.write_deltalake(dt_loc, at, partition_by=self.partition_by, mode="append")
 
         with self.subTest("should aggree", mode="append"):
-            df_expected = deltalake.DeltaTable(loc)
-            df = xdlake.DeltaTable(loc).to_pyarrow_dataset().to_table()
-            assert_arrow_table_equal(df_expected, df)
+            assert_arrow_table_equal(deltalake.DeltaTable(xd_loc), xdlake.DeltaTable(dt_loc).to_pyarrow_table())
 
         with self.subTest("should aggree", mode="overwrite"):
-            xdlake.Writer.write(loc, self.gen_table(), partition_by=self.partition_by, mode="overwrite")
-            df_expected = deltalake.DeltaTable(loc)
-            df = xdlake.DeltaTable(loc).to_pyarrow_dataset().to_table()
-            assert_arrow_table_equal(df_expected, df)
+            xdlake.Writer.write(xd_loc, overwrite_arrow_table, partition_by=self.partition_by, mode="overwrite")
+            deltalake.write_deltalake(dt_loc, overwrite_arrow_table, partition_by=self.partition_by, mode="overwrite")
+            assert_arrow_table_equal(deltalake.DeltaTable(xd_loc), xdlake.DeltaTable(dt_loc).to_pyarrow_table())
 
     def test_schema_change(self):
-        loc = f"{self.scratch_folder}/{uuid4()}"
-        dl_loc = f"{self.scratch_folder}/{uuid4()}"
+        xd_loc = f"{self.scratch_folder}/{uuid4()}"
+        dt_loc = f"{self.scratch_folder}/{uuid4()}"
 
         tables = [self.gen_table() for _ in range(3)]
         table_new_schema = self.gen_table(additional_cols=["new_column"])
 
         for t in tables:
-            xdlake.Writer.write(loc, t, mode="append")
-            deltalake.write_deltalake(dl_loc, t, mode="append")
+            xdlake.Writer.write(xd_loc, t, mode="append")
+            deltalake.write_deltalake(dt_loc, t, mode="append")
 
-        with self.subTest("should raise"):
+        with self.subTest("xdlake should raise"):
             with self.assertRaises(ValueError):
-                xdlake.Writer.write(loc, table_new_schema, mode="append")
+                xdlake.Writer.write(xd_loc, table_new_schema, mode="append")
 
         with self.subTest("should work"):
-            xdlake.Writer.write(loc, table_new_schema, mode="append", schema_mode="merge")
-            deltalake.write_deltalake(dl_loc, table_new_schema, mode="append", schema_mode="merge", engine="rust")
+            xdlake.Writer.write(xd_loc, table_new_schema, mode="append", schema_mode="merge")
+            deltalake.write_deltalake(dt_loc, table_new_schema, mode="append", schema_mode="merge", engine="rust")
 
         assert_arrow_table_equal(
-            deltalake.DeltaTable(dl_loc).to_pyarrow_table(),
-            xdlake.DeltaTable(loc).to_pyarrow_dataset().to_table(),
+            deltalake.DeltaTable(xd_loc).to_pyarrow_table(),
+            xdlake.DeltaTable(dt_loc).to_pyarrow_dataset().to_table(),
         )
 
 
